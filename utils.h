27bceb367c80f32ac5b24f192f927cdc040c793c
mario-kumag <146040723+mario-kumag@users.noreply.github.com>
2024-11-19 18:27:45 -0300

    version final

--- utils.h
+++ utils.h
@@ -1,6 +1,22 @@
+/**
+ * @file utils.h
+ * @brief Funciones auxiliares para el manejo de cadenas.
+ *
+ * Este archivo declara utilidades generales que se utilizan en el sistema,
+ * como la conversion de cadenas a minusculas.
+ */
+
 #ifndef UTILS_H
 #define UTILS_H
 
+/**
+ * @brief Convierte una cadena de caracteres a minusculas.
+ *
+ * Recorre cada caracter de la cadena y lo transforma a su equivalente en minuscula.
+ * Modifica la cadena en su lugar.
+ *
+ * @param str Puntero a la cadena que se desea convertir.
+ */
 void convertirAMinusculas(char *str);
 
 #endif
