17233902a6a320e3a7c23ccddae8211df688f3ab
Constanza <conaraya@umag.cl>
2024-11-19 18:27:49 -0300

    Update utils.c
    documentado y listo:)

--- utils.c
+++ utils.c
@@ -1,6 +1,22 @@
+/**
+ * @file utils.c
+ * @brief Utilidades generales para el sistema de manejo de documentos.
+ *
+ * Este archivo contiene funciones auxiliares que se utilizan en diversas partes
+ * del sistema, como la conversion de cadenas a minusculas.
+ */
+
 #include "utils.h"
 #include <ctype.h>
 
+/**
+ * @brief Convierte una cadena de caracteres a minusculas.
+ *
+ * Recorre cada caracter de la cadena y lo transforma a su equivalente en minuscula.
+ * Modifica la cadena en su lugar.
+ *
+ * @param str Puntero a la cadena que se desea convertir.
+ */
 void convertirAMinusculas(char *str) {
     while (*str) {
         *str = tolower(*str);
